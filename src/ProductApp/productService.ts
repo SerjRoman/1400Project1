6a01726061f9a4860251dd25c631365fc8d5c7bb
DanilKoshnarev <146003208+DanilKoshnarev@users.noreply.github.com>
2025-02-12 15:41:49 +0200

    Update productService.ts

--- src/ProductApp/productService.ts
+++ src/ProductApp/productService.ts
@@ -1,61 +1,22 @@
-import productRepository from "./productRepository";
-import { Prisma } from "@prisma/client";
+import productRepository from './ProductRepository';
+import { ICreateProduct, IProductResponse } from './types';
 
-type Product = Prisma.ProductGetPayload<{}>
-
-interface IProductError{
-    status: 'error',
-    message: string
+async function getAllProducts(): Promise<IProductResponse> {
+    return await productRepository.getAllProducts();
 }
 
-interface IProductsSuccess{
-    status: 'success',
-    data: Product[]
+async function getProductById(id: number): Promise<IProductResponse> {
+    return await productRepository.getProductById(id);
 }
 
-interface IProductSuccess{
-    status: 'success',
-    data: Product
-}
-
-async function getAllProducts(): Promise< IProductsSuccess | IProductError >{
-    
-    const products = await productRepository.getAllProducts()
-    // if (max <= products.length) {
-    //     context.products = products.slice(0, max)
-    // }
-
-    if (!products){
-        return {status: 'error', message: 'products not found'};
-    }
-    return {status: 'success', data: products};
-}
-
-async function getProductById(id: number): Promise< IProductSuccess | IProductError > {
-    let product = await productRepository.getProductById(id)
-
-    if (!product) {
-        return {status: 'error', message: 'product not found'}
-    }
-
-    return {status: 'success', data: product}
-    
-}
-
-
-async function createProduct(data: Prisma.ProductCreateInput): Promise< IProductSuccess | IProductError >{
-    let product = await productRepository.createProduct(data);
-    if (!product){
-        return {status: "error", message: "product create error"}
-    }
-
-    return {status: "success", data: product}
+async function createProduct(data: ICreateProduct): Promise<IProductResponse> {
+    return await productRepository.createProduct(data);
 }
 
 const productService = {
-    getAllProducts: getAllProducts,
-    getProductById: getProductById,
-    createProduct: createProduct
-} 
+    getAllProducts,
+    getProductById,
+    createProduct
+};
 
-export default productService
+export default productService;
