0649a07e1138af5cb266eac2c86e9f584787f1ee
DanilKoshnarev <146003208+DanilKoshnarev@users.noreply.github.com>
2025-02-12 15:31:48 +0200

    Update userController.ts

--- src/UserApp/userController.ts
+++ src/UserApp/userController.ts
@@ -1,54 +1,22 @@
-import express, { Express, Request, Response } from 'express'
-import userService from './userService'
-import { SECRET_KEY } from '../config/token'
-import { sign } from 'jsonwebtoken'
+import { Request, Response } from 'express';
+import userService from './UserService';
+import { sign } from 'jsonwebtoken';
+import { SECRET_KEY } from '../config';
 
-function login(req: Request, res: Response){
-    res.render('login')
-}
-
-function registration(req: Request, res: Response){
-    res.render('registration')
-}
-
-async function authUser(req: Request, res: Response){
-    // console.log(req.body)
-    // // метод cookie отправляет специальный заголовок Set-Cookie
-    // res.cookie('user', req.body.email)
-    // res.sendStatus(200)
-
-    const data = req.body
-    const user = await userService.authUser(data.email, data.password)
-    
-    if (user.status == 'error'){
-        res.send(user.message)
-        return 
+async function registerUser(req: Request, res: Response) {
+    const data = req.body;
+    const result = await userService.registerUser(data);
+    if (result.status === 'error') {
+        res.status(400).send(result.message);
+        return;
     }
-
-    const token = sign(user.data, SECRET_KEY, {expiresIn: '1h'})
-    res.cookie('token', token)
-    res.sendStatus(200)
+    const token = sign(result.data, SECRET_KEY, { expiresIn: '1h' });
+    res.cookie('token', token);
+    res.sendStatus(200);
 }
-
-async function registerUser(req: Request, res: Response){
-    const data = req.body
-    const result = await userService.registerUser(data)
-    if (result.status == 'error'){
-        res.send(result.message)
-        return
-    }
-    const token = sign(result.data, SECRET_KEY, {expiresIn: '1h'})
-    res.cookie('token', token)
-    res.sendStatus(200)
-}
-
-
 
 const userController = {
-    login: login,
-    authUser: authUser,
-    registration: registration,
-    registerUser: registerUser,
-}
+    registerUser,
+};
 
-export default userController
+export default userController;
