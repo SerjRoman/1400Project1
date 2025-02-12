e5ed912648b04fecb4db732a765f38d890c2810b
DanilKoshnarev <146003208+DanilKoshnarev@users.noreply.github.com>
2025-02-12 15:48:31 +0200

    Update productRouter.ts

--- src/ProductApp/productRouter.ts
+++ src/ProductApp/productRouter.ts
@@ -1,19 +1,10 @@
-// const express = require('express')
-// const router = express.Router()
-// const productController = require('../controllers/productController')
-// router.get('/all', productController.getAllProducts)
-// router.get('/:id', productController.getProductById)
-// router.post('/create', productController.createProduct)
-// module.exports = router
-import { authMiddleware } from '../middlewares/authMiddleware';
-import { userRoleMiddleware } from '../middlewares/userRoleMiddleware';
-import productControllers from './productController';
-import {Router} from 'express';
+import { Router } from 'express';
+import productController from './ProductController';
 
 const router = Router();
 
-router.get('/', productControllers.getAllProducts);
-router.get('/:id', productControllers.getProductById); 
-router.get('/:category/createProduct', authMiddleware, userRoleMiddleware, productControllers.renderCreateProduct)
+router.get('/products', productController.getAllProducts);
+router.get('/products/:id', productController.getProductById);
+router.post('/products', productController.createProduct);
 
 export default router;
