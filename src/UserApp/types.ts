7ad49f3af152d25e810d76e98d1fd0588cdf5aa8
SerjRoman <sergeyroman26@gmail.com>
2025-03-09 13:59:50 +0000

    type fix

--- src/UserApp/types.ts
+++ src/UserApp/types.ts
@@ -2,10 +2,10 @@ import { Prisma } from "@prisma/client";
 
 export type User = Prisma.UserGetPayload<{
     select: {
-        id: true,
         username: true,
         email: true,
-        role: true
+        role: true,
+        image: true
     }
 }>
 
